87c82883d5370a8aebe7d9e52d143711ae3d93ee
Milton Navarro <milton.j.navarro.p@gmail.com>
2022-10-09 11:03:08 -0600

    how to add an image in flutter

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
-
+//how to add an image in flutter
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
